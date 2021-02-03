25831d1e55f59bccf4e1dad86e409f6cf86c827a
stsh1119 <stshlaptop@gmail.com>
2021-02-03 23:39:33 +0200

    + access, refresh tokens

--- fork/__init__.py
+++ fork/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fork.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.environ.get('FORKS_KEY')
+
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 from fork import routes # noqa
--- fork/routes.py
+++ fork/routes.py
@@ -1,8 +1,47 @@
-from flask import jsonify
-from fork import app
-from fork.models import Fork
+from flask import jsonify, request
+from flask_jwt_extended import (
+    jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity
+)
+from fork import app, bcrypt
+import datetime
 
 
 @app.route('/')
 def index():
     return jsonify("hello"), 200
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if username != 'test' or password != 'test':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_expiry = datetime.timedelta(minutes=1)
+    reftesh_expiry = datetime.timedelta(days=200)
+    response = {
+        'access_token': create_access_token(identity=username, expires_delta=access_expiry),
+        'refresh_token': create_refresh_token(identity=username, expires_delta=reftesh_expiry)
+    }
+    return jsonify(response), 200
+
+
+@app.route('/refresh', methods=['POST'])
+@jwt_refresh_token_required
+def refresh():
+    current_user = get_jwt_identity()
+    access_expiry = datetime.timedelta(minutes=1)
+    response = {
+        'access_token': create_access_token(identity=current_user, expires_delta=access_expiry),
+    }
+    return jsonify(response), 200
+
+
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    username = get_jwt_identity()
+    return jsonify(logged_in_as=username), 200
