c51e2588057dd7cba3cdad83ed51b04a54db9728
stsh1119 <stshlaptop@gmail.com>
2021-02-07 21:59:54 +0200

    [+] get all forks

--- fork/main/routes.py
+++ fork/main/routes.py
@@ -1,6 +1,7 @@
 from flask import jsonify, request, Blueprint
 from flask_jwt_extended import jwt_required
 from fork.models import Fork
+from fork.utils import prettify_fork
 
 main = Blueprint('main', __name__)
 
@@ -9,5 +10,6 @@ main = Blueprint('main', __name__)
 @jwt_required
 def get_all_forks():
     page = request.args.get('page', default=1, type=int)
-    forks = Fork.query.order_by(Fork.creation_date.desc()).paginate(page=page, per_page=10)
-    return jsonify(fork.items for fork in forks)
+    forks = Fork.query.order_by(Fork.creation_date.desc()).paginate(page=page, per_page=10).items
+    forks = prettify_fork(forks)
+    return jsonify(forks)
--- fork/utils.py
+++ fork/utils.py
@@ -10,3 +10,18 @@ def validate_user_email(email):
     except EmailNotValidError:
         email = ''
         return email
+
+
+def prettify_fork(forks: list):
+    forks_list = []
+    for fork in forks:
+        fork = {
+            'id': fork.fork_id,
+            'name': fork.name,
+            'description': fork.description,
+            'creation_date': fork.creation_date,
+            'category': fork.fork_category,
+            'owner': fork.user,
+        }
+        forks_list.append(fork)
+    return forks_list
