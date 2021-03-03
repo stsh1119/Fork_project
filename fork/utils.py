353623211e3f51f32ddff094617cbbe8ab1fe093
stsh1119 <stshlaptop@gmail.com>
2021-03-03 21:25:03 +0200

    [-] prettify_forks, prettify_categories

--- fork/utils.py
+++ fork/utils.py
@@ -16,34 +16,6 @@ def validate_user_email(email):
         return email
 
 
-def prettify_forks(forks: list) -> list:
-    """Takes db output(queries related to forks) and serializes it, so that it can be returned"""
-    forks_list = []
-    for fork in forks:
-        fork = {
-            'id': fork.fork_id,
-            'name': fork.name,
-            'description': fork.description,
-            'creation_date': fork.creation_date,
-            'category': fork.fork_category,
-            'owner': fork.user,
-        }
-        forks_list.append(fork)
-    return forks_list
-
-
-def prettify_categories(categories: list) -> list:
-    """Takes db output(queries related to fork categories) and serializes it, so that it can be returned"""
-    categories_list = []
-    for category in categories:
-        category = {
-            'name': category.category,
-            'description': category.description,
-        }
-        categories_list.append(category)
-    return categories_list
-
-
 def prepare_creation_data(raw_data):
     """"Categories  are going to be predefined, so that users can only choose from suggested ones,
         this will be marked as 'Uncategorized' otherwise.
